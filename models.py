06bdbf2130f225290081eb9ac35fd29f2d08acbf
Francis Brokering <francisbrokering@gmail.com>
2022-05-01 22:37:38 -0700

    database implimentation

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ This file defines the database models
 """
 
 import datetime
+from email.policy import default
 from .common import db, Field, auth
 from pydal.validators import *
 
@@ -14,10 +15,34 @@ def get_time():
     return datetime.datetime.utcnow()
 
 
-### Define your table below
-#
-# db.define_table('thing', Field('name'))
-#
-## always commit your models to avoid problems later
+db.define_table(
+    'tutor',
+    Field('first_name', 'string', requires=IS_NOT_EMPTY()),
+    Field('last_name', 'string', requires=IS_NOT_EMPTY()),
+    Field('rate', 'string'),
+    Field('user_email', default=get_user_email),
+    Field('bio', 'text')
+)
 
+db.define_table(
+    'classes',
+    Field('class_name', 'string', requires=IS_NOT_EMPTY()),
+    Field('professor', 'string'), #professor they had when taking the class
+    Field('user_email', default=get_user_email),
+)
+
+#linkes tutors with classes they've taken
+db.define_table(
+    'class_to_tutor',
+    Field('tutor', 'reference tutor'),
+    Field('classes', 'reference classes'),
+)
+
+
+
+db.tutor.user_email.readable = db.tutor.user_email.writable = False
+db.classes.user_email.readable = db.classes.user_email.writable = False
+db.tutor.id.readable = db.tutor.id.writable = False
+db.classes.id.readable = db.classes.id.writable = False
+# db.classes.tutor_id.readable = db.classes.tutor_id.writable = False
 db.commit()
