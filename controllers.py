44af4e2f7640eade8228c1b5536812e257521ef1
nateelee <nathaniel902@gmail.com>
2022-05-06 14:16:46 -0700

    implmemented new db with new tutor contact info page -nl

--- controllers.py
+++ controllers.py
@@ -25,16 +25,76 @@ session, db, T, auth, and tempates are examples of Fixtures.
 Warning: Fixtures MUST be declared with @action.uses({fixtures}) else your app will result in undefined behavior
 """
 
-from py4web import action, request, abort, redirect, URL
+from py4web import action, request, abort, redirect, URL, Field
 from yatl.helpers import A
 from .common import db, session, T, cache, auth, logger, authenticated, unauthenticated, flash
 from py4web.utils.url_signer import URLSigner
 from .models import get_user_email
+from py4web.utils.form import Form, FormStyleBulma
+from pydal.validators import *
+
+from .models import get_user_email
 
 url_signer = URLSigner(session)
 
-@action('index')
+@action('index',  method = ["GET", "POST"])
 @action.uses(db, auth, 'index.html')
 def index():
     print("User:", get_user_email())
-    return dict()
+    form = Form(db.classes, deletable = False ,csrf_session=session, formstyle=FormStyleBulma)
+    if form.accepted:
+        # update happened, redirect
+        redirect(URL('index'))
+    return dict(form = form)
+
+# extend this to the sign on page /edit profile
+@action('create_tutor', method = ["GET", "POST"])
+@action.uses(db, auth.user, 'create_tutor.html')
+def create_tutor():
+    form = Form(
+        db.tutors,
+        deletable = False,
+        csrf_session=session, 
+        formstyle=FormStyleBulma
+    )
+    
+    
+    if form.accepted:
+        # update happened, redirect
+        tutor_id = db(db.tutors.user_email == get_user_email()).select()[0].id
+        print(db(db.tutors.user_email).select())
+        redirect(URL('tutor_add_class', tutor_id))
+    return dict(form = form)
+
+# need to restrict this to only those who are signed in
+@action('tutor_add_class/<tutor_id:int>', method = ["GET", "POST"])
+@action.uses(db, auth, 'tutor_add_class.html')
+def tutor_add_class(tutor_id = None):
+    assert tutor_id is not None
+    print(tutor_id)
+    query = db(db.classes).select().as_list()
+    
+    class_set = []
+    class_id = {}
+    for table in query:
+        class_set.append(table['class_name'])
+        class_id[table['class_name']] = table['id']
+
+    form = Form(
+        [
+            Field('class_name', requires=IS_IN_SET(class_set)),
+        ], 
+        deletable = False,
+        csrf_session=session, 
+        formstyle=FormStyleBulma
+    )
+    
+    if form.accepted:
+        # update happened, redirect
+      
+        db.class_to_tutor.insert(
+            tutor = tutor_id,
+            class_id = class_id[form.vars['class_name']]
+        )
+        redirect(URL('index'))
+    return dict(form = form)
--- models.py
+++ models.py
@@ -19,5 +19,35 @@ def get_time():
 # db.define_table('thing', Field('name'))
 #
 ## always commit your models to avoid problems later
+db.define_table(
+    'tutors',
+    Field('first_name', 'string', requires=IS_NOT_EMPTY()),
+    Field('last_name', 'string', requires=IS_NOT_EMPTY()),
+    Field('rate', 'string'),
+    Field('user_email', default=get_user_email),
+    Field('bio', 'text')
+)
 
+db.define_table(
+    'classes',
+    Field('class_name', 'string', requires=IS_NOT_EMPTY()),
+    Field('professor', 'string'), #professor they had when taking the class
+    Field('user_email', default=get_user_email),
+)
+
+#linkes tutors with classes they've taken
+db.define_table(
+    'class_to_tutor',
+    Field('tutor', 'reference tutors'),
+    Field('class_id', 'reference classes'),
+)
+
+
+
+db.tutors.user_email.readable = db.tutors.user_email.writable = False
+db.classes.user_email.readable = db.classes.user_email.writable = False
+db.tutors.id.writable = False
+db.classes.id.writable = False
+# db.classes.tutor_id.readable = db.classes.tutor_id.writable = False
 db.commit()
+
